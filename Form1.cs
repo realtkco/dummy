580fc944e4ca317ee66bded707058d9b297d35af
wiggy <coltenlange@gmail.com>
2024-08-12 04:59:20 -0500

    Initial Release

---
+++ Form1.cs
@@ -0,0 +1,16 @@
+namespace Dummy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Text = "Dummy.exe";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
